c29372b76d5a013245fd0ee7d686bc27f719f176
Thoriq Ardiansyah <thoriq.pieces11@gmail.com>
2024-08-09 10:30:38 +0700

    fix bugs

--- config/config.js
+++ config/config.js
@@ -16,10 +16,7 @@ module.exports = {
     dialect: process.env.DB_DIALECT,
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE_NAME,
-    host: process.env.DB_HOST,
+    use_env_variable: process.env.DATABASE_URL,
     dialect: process.env.DB_DIALECT,
   },
 };
